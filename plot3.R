67c82e2ee4872ef0c2022d5bc5c2973f7502b0f6
Hesham A. Amin <hspceng@gmail.com>
2014-12-06 15:22:57 +0200

    Adding plot 3

---
+++ plot3.R
@@ -0,0 +1,14 @@
+system("head --lines 1 ./data/household_power_consumption.txt  > ./data/filtered.txt")
+system("cat ./data/household_power_consumption.txt | grep -e ^1/2/2007 -e ^2/2/2007 >> ./data/filtered.txt")
+
+
+elec <- read.table(file = "./data/filtered.txt", header = T, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
+
+elec$DateTime <- strptime(paste(elec$Date,elec$Time,sep="-"), format = "%d/%m/%Y-%H:%M:%S")
+
+png(filename = "plot3.png", width = 480, height = 480)
+plot(x=elec$DateTime,y= elec$Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l")
+lines(x=elec$DateTime,y= elec$Sub_metering_2, type = "l", col = "red")
+lines(x=elec$DateTime,y= elec$Sub_metering_3, type = "l", col = "purple")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "purple"), lty = c(1,1))
+dev.off()
